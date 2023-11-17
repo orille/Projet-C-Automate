5423371e3dee70ce0521dca8ff4bbe6cdaaeaab0
MohamedBMS38 <151142681+MohamedBMS38@users.noreply.github.com>
2023-11-17 11:20:58 +0100

    Add files via upload

--- aef.c
+++ aef.c
@@ -86,24 +86,37 @@ void deleteFSM(FiniteStateMachine *fsm) {
 }
 
 int main() {
-    int states, symbols;
-    printf("Nombre d'états : ");
-    scanf("%d", &states);
-    printf("Nombre de symboles : ");
-    scanf("%d", &symbols);
+    int choice;
+    printf("Choisir une option :\n");
+    printf("1. Saisir une nouvelle FSM\n");
+    printf("2. Charger une FSM depuis un fichier\n");
+    printf("Votre choix : ");
+    scanf("%d", &choice);
 
-    FiniteStateMachine *fsm = createFSM(states, symbols);
-    inputFSM(fsm);
-    printFSM(fsm);
+    FiniteStateMachine *fsm;
 
-    saveFSMToFile(fsm, "fsm.txt");
+    if (choice == 1) {
+        int states, symbols;
+        printf("Nombre d'états : ");
+        scanf("%d", &states);
+        printf("Nombre de symboles : ");
+        scanf("%d", &symbols);
 
-    FiniteStateMachine *loadedFSM;
-    loadFSMFromFile(&loadedFSM, "fsm.txt");
-    printFSM(loadedFSM);
+        fsm = createFSM(states, symbols);
+        inputFSM(fsm);
+        printFSM(fsm);
+        saveFSMToFile(fsm, "fsm.txt");
+    } else if (choice == 2) {
+        char filename[100];
+        printf("Entrez le nom du fichier à charger : ");
+        scanf("%s", filename);
+        loadFSMFromFile(&fsm, filename);
+        printFSM(fsm);
+    } else {
+        printf("Choix non valide.\n");
+        return 1;
+    }
 
     deleteFSM(fsm);
-    deleteFSM(loadedFSM);
-
     return 0;
 }
