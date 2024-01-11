f98a74aa1fda95540f82d09ca5b963368876dec7
orille <96992185+orille@users.noreply.github.com>
2024-01-11 18:07:38 +0100

    Update fsm.h

--- fsm.h
+++ fsm.h
@@ -5,17 +5,17 @@
 #define FSM_H
 // définition de cette variavble FSM_H, d'autres solutions à ce problème sont possibles comme #pragma once" mais ne fonctionne qu'avec certains compilateurs
 
-typedef struct {
-    int nombre_etats;
+typedef struct {    // définition de la structure FiniteStateMachine à l'aide de typedef car typedef est une écriture moins lourde et plus lisible que 'struct FiniteStateMachgine'
+    int nombre_etats;    // définition des variables structurées pour le nombre d'états, le nombre de symboles et un pointeur de pointeur pour la matrice de transition
     int nombre_symboles;
     int **matrice_transition;
 } FiniteStateMachine;
 
-FiniteStateMachine *creationFSM(int etats, int symboles);
-void saisieFSM(FiniteStateMachine *fsm);
+FiniteStateMachine *creationFSM(int etats, int symboles);    // la fsm accede à la valeur de l'objet créé par la fonction creationFSM et ses proprités saisies ou importées
+void saisieFSM(FiniteStateMachine *fsm);    //   
 void affichageFSM(FiniteStateMachine *fsm);
 void sauvegardeFSMfichier(FiniteStateMachine *fsm, const char *nom_fichier);
 void chargerFSMfichier(FiniteStateMachine **fsm, const char *nom_fichier);
 void supprFSM(FiniteStateMachine *fsm);
 
-#endif // FSM_H
+#endif // fin de la condition si FSH_H n'est pas définie
