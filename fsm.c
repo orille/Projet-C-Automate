29168dd0c309f7625103d8e454c7e1b3e88e6fe6
orille <96992185+orille@users.noreply.github.com>
2024-01-11 18:12:24 +0100

    Update fsm.c

--- fsm.c
+++ fsm.c
@@ -3,81 +3,81 @@
 #include <stdlib.h>
 #include "fsm.h"
 
-FiniteStateMachine *createFSM(int states, int symbols) {
+FiniteStateMachine *creationFSM(int etats, int symboles) {
     FiniteStateMachine *fsm = malloc(sizeof(FiniteStateMachine));
-    fsm->numberOfStates = states;
-    fsm->numberOfSymbols = symbols;
-    fsm->transitionMatrix = malloc(states * sizeof(int *));
-    for (int i = 0; i < states; i++) {
-        fsm->transitionMatrix[i] = malloc(symbols * sizeof(int));
-        for (int j = 0; j < symbols; j++) {
-            fsm->transitionMatrix[i][j] = -1;
+    fsm->nombre_etats = etats;
+    fsm->nombre_symboles = symboles;
+    fsm->matrice_transition = malloc(etats * sizeof(int *));
+    for (int i = 0; i < etats; i++) {
+        fsm->matrice_transition[i] = malloc(symboles * sizeof(int));
+        for (int j = 0; j < symboles; j++) {
+            fsm->matrice_transition[i][j] = -1;
         }
     }
     return fsm;
 }
 
-void inputFSM(FiniteStateMachine *fsm) {
+void saisieFSM(FiniteStateMachine *fsm) {
     printf("Entrer les transitions pour chaque état et symbole :\n");
-    for (int i = 0; i < fsm->numberOfStates; i++) {
-        for (int j = 0; j < fsm->numberOfSymbols; j++) {
+    for (int i = 0; i < fsm->nombre_etats; i++) {
+        for (int j = 0; j < fsm->nombre_symboles; j++) {
             printf("Transition de l'état %d avec le symbole %d: ", i, j);
-            scanf("%d", &fsm->transitionMatrix[i][j]);
+            scanf("%d", &fsm->matrice_transition[i][j]);
         }
     }
 }
 
-void printFSM(FiniteStateMachine *fsm) {
+void affichageFSM(FiniteStateMachine *fsm) {
     printf("Matrice de transition de la FSM :\n");
-    for (int i = 0; i < fsm->numberOfStates; i++) {
-        for (int j = 0; j < fsm->numberOfSymbols; j++) {
-            printf("%d ", fsm->transitionMatrix[i][j]);
+    for (int i = 0; i < fsm->nombre_etats; i++) {
+        for (int j = 0; j < fsm->nombre_symboles; j++) {
+            printf("%d ", fsm->matrice_transition[i][j]);
         }
         printf("\n");
     }
 }
 
-void saveFSMToFile(FiniteStateMachine *fsm, const char *filename) {
-    FILE *file = fopen(filename, "w");
-    if (file == NULL) {
+void sauvegardeFSMfichier(FiniteStateMachine *fsm, const char *nom_fichier) {
+    FILE *fichier = fopen(nom_fichier, "w");
+    if (fichier == NULL) {
         printf("Erreur lors de l'ouverture du fichier.\n");
         return;
     }
-    fprintf(file, "%d %d\n", fsm->numberOfStates, fsm->numberOfSymbols);
-    for (int i = 0; i < fsm->numberOfStates; i++) {
-        for (int j = 0; j < fsm->numberOfSymbols; j++) {
-            fprintf(file, "%d ", fsm->transitionMatrix[i][j]);
+    fprintf(fichier, "%d %d\n", fsm->nombre_etats, fsm->nombre_symboles);
+    for (int i = 0; i < fsm->nombre_etats; i++) {
+        for (int j = 0; j < fsm->nombre_symboles; j++) {
+            fprintf(fichier, "%d ", fsm->matrice_transition[i][j]);
         }
-        fprintf(file, "\n");
+        fprintf(fichier, "\n");
     }
-    fclose(file);
-    printf("FSM enregistrée dans le fichier '%s'.\n", filename);
+    fclose(fichier);
+    printf("FSM enregistrée dans le fichier '%s'.\n", nom_fichier);
 }
 
 
-void loadFSMFromFile(FiniteStateMachine **fsm, const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
+void chargerFSMfichier(FiniteStateMachine **fsm, const char *nom_fichier) {
+    FICHIER *fichier = fopen(nom_fichier, "r");
+    if (fichier == NULL) {
         printf("Erreur lors de l'ouverture du fichier.\n");
         return;
     }
-    int states, symbols;
-    fscanf(file, "%d %d", &states, &symbols);
-    *fsm = createFSM(states, symbols);
-    for (int i = 0; i < states; i++) {
-        for (int j = 0; j < symbols; j++) {
-            fscanf(file, "%d", &(*fsm)->transitionMatrix[i][j]);
+    int etats, symboles;
+    fscanf(fichier, "%d %d", &etats, &symboles);
+    *fsm = creationFSM(etats, symboles);
+    for (int i = 0; i < etats; i++) {
+        for (int j = 0; j < symboles; j++) {
+            fscanf(fichier, "%d", &(*fsm)->matrice_transition[i][j]);
         }
     }
-    fclose(file);
-    printf("FSM chargée depuis le fichier '%s'.\n", filename);
+    fclose(fichier);
+    printf("FSM chargée depuis le fichier '%s'.\n", nom_fichier);
 }
 
-void deleteFSM(FiniteStateMachine *fsm) {
-    for (int i = 0; i < fsm->numberOfStates; i++) {
-        free(fsm->transitionMatrix[i]);
+void supprFSM(FiniteStateMachine *fsm) {
+    for (int i = 0; i < fsm->nombre_etats; i++) {
+        free(fsm->matrice_transition[i]);
     }
-    free(fsm->transitionMatrix);
+    free(fsm->matrice_transition);
     free(fsm);
     printf("FSM supprimée.\n");
 }
