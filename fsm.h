ef8d0e81b6be3fbe80ca0ce4cb7d8cb3feef4002
orille <96992185+orille@users.noreply.github.com>
2024-01-11 17:07:28 +0100

    Update fsm.h

--- fsm.h
+++ fsm.h
@@ -1,7 +1,9 @@
 // fsm.h
-
-#ifndef FSM_H
+// Ce fichier .h (header) contient les prototypes des fonctions de la fsm
+// Le fichier fsm.c contient lui les définitions de ces foncions
+#ifndef FSM_H    // utilisation de ifndef pour éviter problème si ce fichier de prototype est importé dans plusieurs fichier .h à la fois, ici si la variable FSM_H n'est pas définie alors le header sera inclus sinon il sera vide
 #define FSM_H
+// définition de cette variavble FSM_H, d'autres solutions à ce problème sont possibles comme #pragma once" mais ne fonctionne qu'avec certains compilateurs
 
 typedef struct {
     int nombre_etats;
