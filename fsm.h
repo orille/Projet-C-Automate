8e53296e294109522bf3d2e049c3a2ea0872fcc4
orille <96992185+orille@users.noreply.github.com>
2024-01-14 11:13:03 +0100

    Update fsm.h

--- fsm.h
+++ fsm.h
@@ -1,28 +1,26 @@
 // fsm.h
-// Ce fichier .h (header) contient les prototypes des fonctions de la fsm
-// Le fichier fsm.c contient lui les définitions de ces foncions
-#ifndef FSM_H    // utilisation de ifndef pour éviter problème si ce fichier de prototype est importé dans plusieurs fichier .h à la fois, ici si la variable FSM_H n'est pas définie alors le header sera inclus sinon il sera vide
+// This header (.h) file contains the prototypes of the fsm functions
+// The fsm.c file contains the definitions of these functions
+#ifndef FSM_H    // Use of ifndef to avoid issues if this prototype file is included in multiple .h files at the same time; here, if the FSM_H variable is not defined, the header will be included, otherwise, it will be empty.
 #define FSM_H
-// définition de cette variavble FSM_H, d'autres solutions à ce problème sont possibles comme "#pragma once" mais ne fonctionne qu'avec certains compilateurs
+// Definition of the FSM_H variable; other solutions to this problem are possible, such as "#pragma once," but it only works with certain compilers
 
-
-typedef struct {    // définition de la structure FiniteStateMachine à l'aide de typedef car typedef est une écriture moins lourde et plus lisible que 'struct FiniteStateMachgine'
-    int nombre_etats;    // définition des variables structurées pour le nombre d'états, le nombre de symboles, un pointeur de pointeur pour la matrice de transition, un nb d'états accepteur et un etat accepteur (pour la vérif du mot mais on n'a pas réussi)
+typedef struct {    // Definition of the FiniteStateMachine structure using typedef because typedef is a less cumbersome and more readable way than 'struct FiniteStateMachine'
+    int nombre_etats;    // Definition of structured variables for the number of states, the number of symbols, a pointer to a pointer for the transition matrix, an accepting state count, and an accepting state (for word verification, but we didn't succeed)
     int nombre_symboles; 
     int **matrice_transition;
     int etats_accepteurs;
     int nb_etats_accepteurs;
 } FiniteStateMachine;
 
+FiniteStateMachine *creationFSM(int etats, int symboles);    // The creationFSM function will dynamically allocate memory to create an instance of the FiniteStateMachine structure and return a pointer to this instance
+void saisieFSM(FiniteStateMachine *fsm);    // The prototype of the saisieFSM function takes a pointer to a created FiniteStateMachine structure as an argument and does not return any value
+void afficherFSM(FiniteStateMachine *fsm);    // The prototype of the afficherFSM function takes a pointer to a created FiniteStateMachine structure as an argument and does not return any value
+void sauvegardeFSMfichier(FiniteStateMachine *fsm, const char *nom_fichier);    // The prototype of the sauvegardeFSMfichier function takes a pointer to a created FiniteStateMachine structure and the name of the text file (constant and not modifiable) that will be created to save this fsm
+void chargerFSMfichier(FiniteStateMachine **fsm, const char *nom_fichier);    // The prototype of the chargerFSMfichier function takes a pointer to a pointer to a created FiniteStateMachine structure (allows modifying the original pointer to the fsm to dynamically allocate memory for the struct or its data) and the name of the text file (constant and not modifiable) containing the fsm to be loaded
+void supprFSM(FiniteStateMachine *fsm);     // The prototype of the supprFSM function takes a pointer to a created FiniteStateMachine structure as an argument and does not return any value
+bool VerifComplet(FiniteStateMachine *fsm);        // The prototype of the VerifComplet function takes a pointer to a created FiniteStateMachine structure as an argument and returns a boolean (true or false)
+void CompleterFSM(FiniteStateMachine *fsm);    // The prototype of the CompleterFSM function takes a pointer to a created FiniteStateMachine structure as an argument and does not return any value
+bool estReconnu(FiniteStateMachine* fsm, const char* mot);    // The prototype of the estReconnu function takes a pointer to a created FiniteStateMachine structure and the word to be recognized as arguments and returns a boolean (true or false)
 
-FiniteStateMachine *creationFSM(int etats, int symboles);    // la fonction creationFSM va allouer dynamiquement de la mémoire pour crééer une instance de la structure FiniteStateMachine et renverra un pointeur vers cette instance 
-void saisieFSM(FiniteStateMachine *fsm);    // le prototype de la fonction saisieFSM prend en argument un pointeur vers une strucure FiniteStateMAchine créée et ne retourne aucune valeur   
-void afficherFSM(FiniteStateMachine *fsm);    // le prototype de la fonction affichageFSM prend en argument un pointeur vers une strucure FiniteStateMAchine créée et ne retourne aucune valeur 
-void sauvegardeFSMfichier(FiniteStateMachine *fsm, const char *nom_fichier);    // le prototype de la fonction sauvegardeFSMfichier prend en argument un pointeur vers une strucure FiniteStateMAchine créée et le nom du fichier texte (constante donc non modifiable) qui sera créée pour sauvegarder cette fsm
-void chargerFSMfichier(FiniteStateMachine **fsm, const char *nom_fichier);    // le prototype de la fonction chargerFSMfichier prend en argument un pointeur de pointeur vers une strucure FiniteStateMAchine créée (permet de modifier le pointeur originel vers la fsm pour allouer dynamiquement de la mémoire pour la struct ou ses données) et le nom du fichier texte (constante donc non modifiable) contenant la fsm qui sera chargée
-void supprFSM(FiniteStateMachine *fsm);     // le prototype de la fonction supprFSM prend en argument un pointeur vers une strucure FiniteStateMAchine créée et ne retourne aucune valeur   
-bool VerifComplet(FiniteStateMachine *fsm);        // le prototype de la fonction VerifComplet prend en argument un pointeur vers une strucure FiniteStateMAchine créée et retourne un booléen (true ou false)
-void CompleterFSM(FiniteStateMachine *fsm);    // le prototype de la fonction CompleterFSM prend en argument un pointeur vers une strucure FiniteStateMAchine créée et ne retourne aucune valeur
-bool estReconnu(FiniteStateMachine* fsm, const char* mot);    // le prototype de la fonction estReconnu prend en argument un pointeur vers une strucure FiniteStateMAchine créée et le mot à reconnaitre et retourne un booléen (true ou false)
-
-#endif // fin de la condition si FSH_H n'est pas définie
+#endif // End of the condition if FSM_H is not defined
