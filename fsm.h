6b396c59e8c363616cb890c317447082a48d0510
orille <96992185+orille@users.noreply.github.com>
2024-01-14 10:56:21 +0100

    Update fsm.h

--- fsm.h
+++ fsm.h
@@ -3,14 +3,14 @@
 // Le fichier fsm.c contient lui les définitions de ces foncions
 #ifndef FSM_H    // utilisation de ifndef pour éviter problème si ce fichier de prototype est importé dans plusieurs fichier .h à la fois, ici si la variable FSM_H n'est pas définie alors le header sera inclus sinon il sera vide
 #define FSM_H
-// définition de cette variavble FSM_H, d'autres solutions à ce problème sont possibles comme #pragma once" mais ne fonctionne qu'avec certains compilateurs
+// définition de cette variavble FSM_H, d'autres solutions à ce problème sont possibles comme "#pragma once" mais ne fonctionne qu'avec certains compilateurs
 
 
 typedef struct {    // définition de la structure FiniteStateMachine à l'aide de typedef car typedef est une écriture moins lourde et plus lisible que 'struct FiniteStateMachgine'
-    int nombre_etats;    // définition des variables structurées pour le nombre d'états, le nombre de symboles et un pointeur de pointeur pour la matrice de transition
-    int nombre_symboles;
+    int nombre_etats;    // définition des variables structurées pour le nombre d'états, le nombre de symboles, un pointeur de pointeur pour la matrice de transition, un nb d'états accepteur et un etat accepteur (pour la vérif du mot mais on n'a pas réussi)
+    int nombre_symboles; 
     int **matrice_transition;
-    int* etats_accepteurs;
+    int etats_accepteurs;
     int nb_etats_accepteurs;
 } FiniteStateMachine;
 
@@ -21,8 +21,8 @@ void afficherFSM(FiniteStateMachine *fsm);    // le prototype de la fonction aff
 void sauvegardeFSMfichier(FiniteStateMachine *fsm, const char *nom_fichier);    // le prototype de la fonction sauvegardeFSMfichier prend en argument un pointeur vers une strucure FiniteStateMAchine créée et le nom du fichier texte (constante donc non modifiable) qui sera créée pour sauvegarder cette fsm
 void chargerFSMfichier(FiniteStateMachine **fsm, const char *nom_fichier);    // le prototype de la fonction chargerFSMfichier prend en argument un pointeur de pointeur vers une strucure FiniteStateMAchine créée (permet de modifier le pointeur originel vers la fsm pour allouer dynamiquement de la mémoire pour la struct ou ses données) et le nom du fichier texte (constante donc non modifiable) contenant la fsm qui sera chargée
 void supprFSM(FiniteStateMachine *fsm);     // le prototype de la fonction supprFSM prend en argument un pointeur vers une strucure FiniteStateMAchine créée et ne retourne aucune valeur   
-bool VerifComplet(FiniteStateMachine *fsm);
-void CompleterFSM(FiniteStateMachine *fsm);
-bool estReconnu(const FiniteStateMachine* fsm, const char* mot);
+bool VerifComplet(FiniteStateMachine *fsm);        // le prototype de la fonction VerifComplet prend en argument un pointeur vers une strucure FiniteStateMAchine créée et retourne un booléen (true ou false)
+void CompleterFSM(FiniteStateMachine *fsm);    // le prototype de la fonction CompleterFSM prend en argument un pointeur vers une strucure FiniteStateMAchine créée et ne retourne aucune valeur
+bool estReconnu(FiniteStateMachine* fsm, const char* mot);    // le prototype de la fonction estReconnu prend en argument un pointeur vers une strucure FiniteStateMAchine créée et le mot à reconnaitre et retourne un booléen (true ou false)
 
 #endif // fin de la condition si FSH_H n'est pas définie
